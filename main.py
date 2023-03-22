e7429db82a228d0bea102ec7cf8bbbdd1c2e3aec
Edwin Fernando Vasquez Molina <86685519+IngEdwinV@users.noreply.github.com>
2023-03-22 10:02:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ from qiskit.utils import algorithm_globals
 from qiskit.circuit import Parameter
 from qiskit import QuantumCircuit
 from qiskit.quantum_info import SparsePauliOp
+from qiskit_machine_learning.neural_networks import EstimatorQNN
+from qiskit.circuit import ParameterVector
+from qiskit_machine_learning.neural_networks import SamplerQNN
 
+# Ejemplo simple de EstimatorQNN
 algorithm_globals.random_seed = 42
+
+print("-------------- EstimatorQNN -------------------")
 
 params1 = [Parameter("input1"), Parameter("weight1")]
 qc1 = QuantumCircuit(1)
@@ -12,6 +18,59 @@ qc1.ry(params1[0], 0)
 qc1.rx(params1[1], 0)
 qc1.draw("mpl")
 
+# Circuito
 print(qc1)
 
 observable1 = SparsePauliOp.from_list([("Y" * qc1.num_qubits, 1)])
+
+# Creación Estimator QNN
+estimator_qnn = EstimatorQNN(
+    circuit=qc1, observables=observable1, input_params=[params1[0]], weight_params=[params1[1]]
+)
+print(estimator_qnn)
+
+print("-------------- SamplerQNN -------------------")
+inputs2 = ParameterVector("input", 2)
+weights2 = ParameterVector("weight", 4)
+print(f"input parameters: {[str(item) for item in inputs2.params]}")
+print(f"weight parameters: {[str(item) for item in weights2.params]}")
+
+qc2 = QuantumCircuit(2)
+qc2.ry(inputs2[0], 0)
+qc2.ry(inputs2[1], 1)
+qc2.cx(0, 1)
+qc2.ry(weights2[0], 0)
+qc2.ry(weights2[1], 1)
+qc2.cx(0, 1)
+qc2.ry(weights2[2], 0)
+qc2.ry(weights2[3], 1)
+
+qc2.draw(output="mpl")
+
+print(qc2)
+
+sampler_qnn = SamplerQNN(circuit=qc2, input_params=inputs2, weight_params=weights2)
+sampler_qnn
+
+print(sampler_qnn)
+
+print("--------------How to Run a Forward Pass Set-Up-------------------")
+
+estimator_qnn_input = algorithm_globals.random.random(estimator_qnn.num_inputs)
+estimator_qnn_weights = algorithm_globals.random.random(estimator_qnn.num_weights)
+
+print(
+    f"Number of input features for EstimatorQNN: {estimator_qnn.num_inputs} \nInput: {estimator_qnn_input}"
+)
+print(
+    f"Number of trainable weights for EstimatorQNN: {estimator_qnn.num_weights} \nWeights: {estimator_qnn_weights}"
+)
+
+sampler_qnn_input = algorithm_globals.random.random(sampler_qnn.num_inputs)
+sampler_qnn_weights = algorithm_globals.random.random(sampler_qnn.num_weights)
+
+print(
+    f"Number of input features for SamplerQNN: {sampler_qnn.num_inputs} \nInput: {sampler_qnn_input}"
+)
+print(
+    f"Number of trainable weights for SamplerQNN: {sampler_qnn.num_weights} \nWeights: {sampler_qnn_weights}"
